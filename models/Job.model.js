f5a4b3cf97301bc3d921681db6472e62100608c8
kekonline <kekonline@gmail.com>
2023-08-11 18:10:45 +0200

    done some CRUD of pets

--- models/Job.model.js
+++ models/Job.model.js
@@ -4,7 +4,6 @@ const jobSchema = new mongoose.Schema({
     sittr: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true
     },
     owner: {
         type: mongoose.Schema.Types.ObjectId,
--- routes/owner.routes.js
+++ routes/owner.routes.js
@@ -6,58 +6,95 @@ const Job = require("../models/Job.model.js");
 
 
 // GET "/owner/petlist" => Enseñar vista de mascotas
-router.get("/petlist", async(req, res, next) => {
-try {
-    const allOwnerPets = await Pet.find();
-console.log(allOwnerPets)
+router.get("/petlist", async (req, res, next) => {
+    try {
+        const allOwnerPets = await Pet.find();
 
+        res.render("owner/petlist.hbs", {
+            allOwnerPets
+        })
 
-} catch (error) {
-    next(error);
-}
+    } catch (error) {
+        next(error);
+    }
 
-
-    res.render("owner/petlist.hbs")
 })
 
 // GET "/owner/addPet" => Enseñar vista para añadir mascota
 router.get("/add-pet", (req, res, next) => {
- 
-        res.render("owner/addpet.hbs")
-    })
+
+    res.render("owner/addpet.hbs")
+})
 
 // POST "/owner/addPet" => añade mascota a db
-    router.post("/add-pet", async (req, res, next)=> {
-        const {name, animalType, race, size,dateOfBirth,comment} = req.body
-        console.log(req.session.user._id)
-        if (name === "" ||animalType === "" || size === "" ) {
-           
-         res.status(400).render("/add-pet.hbs", {
-             errorMessage: "name, animal Type,size, fields are required"
-         })
-     return
-        }
-         try {
-             
-             await Pet.create({
-                name,
-                animalType,
-                race,
-                size,
-                dateOfBirth,
-                owner: req.session.user._id,
-                comment
+router.post("/add-pet", async (req, res, next) => {
+    const { name, animalType, race, size, dateOfBirth, comment } = req.body
+    // console.log(req.session.user._id)
+    if (name === "" || animalType === "" || size === "") {
 
-             })
-     
-     
-             res.redirect("/owner/petlist")
-         } catch (error) {
-             next(error)
-         }
-     })
+        res.status(400).render("owner/addpet.hbs", {
+            errorMessage: "name, animal Type,size, fields are required"
+        })
+        return
+    }
+    try {
+
+        await Pet.create({
+            name,
+            animalType,
+            race,
+            size,
+            dateOfBirth,
+            owner: req.session.user._id,
+            comment
+        })
+        res.redirect("/owner/petlist")
+    } catch (error) {
+        next(error)
+    }
+})
 
 
+// GET de "/owner/edit-pet/:petId" => Enseñar vista para editar mascota
+router.get ("/edit-pet/:petId", async (req, res, next) => {
+    try {
+        const petToEdit = await Pet.findById(req.params.petId)
+        res.render("owner/editpet.hbs", {
+            petToEdit,
+        })
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+// POST "/owner/edit-pet/:petId" => añade mascota a db
+router.post("/edit-pet/:petId", async (req, res, next) => {
+    const { name, animalType, race, size, dateOfBirth, comment } = req.body
+    // console.log(req.session.user._id)
+    if (name === "" || animalType === "" || size === "") {
+
+        res.status(400).render("owner/editpet.hbs", {           
+            errorMessage: "name, animal Type,size, fields are required"
+        })
+        return
+    }
+    try {
+
+        // await Pet.create({
+        //     name,
+        //     animalType,
+        //     race,
+        //     size,
+        //     dateOfBirth,
+        //     owner: req.session.user._id,
+        //     comment
+        // })
+        res.redirect("/owner/petlist")
+    } catch (error) {
+        next(error)
+    }
+})
 
 
 
@@ -68,11 +105,6 @@ router.get("/add-pet", (req, res, next) => {
 
 
 
-
-
-
-
-    
 
 
 
